89a3eb6d3c12375464db289a752bc22bf8badb62
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-04-10 04:45:32 +0700

    gratis.py

--- PyroUbot/core/plugins/gratis.py
+++ PyroUbot/core/plugins/gratis.py
@@ -213,7 +213,7 @@ async def bikin_gratis(client, callback_query):
     text_done = f"<b>🔥 {bot.me.mention} ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪꜰᴋᴀɴ ᴅɪ ᴀᴋᴜɴ: <a href=tg://openmessage?user_id={new_client.me.id}>{new_client.me.first_name} {new_client.me.last_name or ''}</a> > <code>{new_client.me.id}</code></b> "
     await bot_msg.edit(text_done)
     try:
-        await new_client.join_chat("PremUbotCH")
+        await new_client.join_chat("Userlogsbott")
     except UserAlreadyParticipant:
         pass
     return await bot.send_message(
