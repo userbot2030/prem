fd2ffb21617ef2938adf7eca1286bc0ef3340ff2
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-05-25 15:48:03 +0700

    sid.py

--- PyroUbot/core/plugins/sid.py
+++ PyroUbot/core/plugins/sid.py
@@ -4,35 +4,34 @@ from PyroUbot import *
 
 
 async def id_cmd(client, message):
-    if len(message.command) < 2:
-        chat_type = message.chat.type
-        if chat_type == ChatType.PRIVATE:
-            user_id = message.chat.id
-            await message.reply_text(
-                f"<b>ɪᴅ</b> <code>{user_id}</code>",
-            )
-        elif chat_type == ChatType.CHANNEL:
-            await message.reply(
-                f"<b>ɪᴅ {message.sender_chat.title} ᴀᴅᴀʟᴀʜ:</b> <code>{message.sender_chat.id}</code>",
-            )
-        elif chat_type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            _id = ""
-            _id += f"\n\n<b>ɪᴅ {message.from_user.first_name} {message.from_user.last_name or ''} ᴀᴅᴀʟᴀʜ:</b> <code>{message.from_user.id}</code>\n\n<b>ɪᴅ {message.chat.title} ᴀᴅᴀʟᴀʜ:</b> <code>{message.chat.id}</code>\n"
-            if message.reply_to_message:
-                _id += f"\n\n<b>ɪᴅ {message.reply_to_message.from_user.first_name} {message.reply_to_message.from_user.last_name or ''} ᴀᴅᴀʟᴀʜ:</b> <code>{message.reply_to_message.from_user.id}</code>\n"
-                file_info = get_file_id(message.reply_to_message)
-                if file_info:
-                    _id += f"\n<b>ɪᴅ {file_info.message_type} ᴀᴅᴀʟᴀʜ:</b> <code>{file_info.file_id}</code>\n"
-            m = message.reply_to_message or message
-            return await m.reply_text(_id)
-    try:
-        chat_id = message.text.split()[1]
-        get = await client.get_chat(chat_id)
-        name = f"{get.title}"
-        if name == "None":
-            get = await client.get_users(chat_id)
-            name = f"{get.first_name} {get.last_name or ''}"
-        msg = f"<b>ɪᴅ {name} ᴀᴅᴀʟᴀʜ:</b> <code>{get.id}</code>"
-        return await message.reply(msg)
-    except Exception as why:
-        return await message.reply(why)
+    text = f"<b><emoji id=5319302904308309833>◎</emoji><a href={message.link}>ᴍᴇssᴀɢᴇ ɪᴅ:</a></b> <code>{message.id}</code>\n"
+
+    if message.chat.type == ChatType.CHANNEL:
+        text += f"<b><emoji id=5319139772860472979>◎</emoji><a href=https://t.me/{message.chat.username}>ᴄʜᴀᴛ ɪᴅ:</a></b> <code>{message.sender_chat.id}</code>\n"
+    else:
+        text += f"<b><emoji id=5318967574736676420>◎</emoji><a href=tg://user?id={message.from_user.id}>ʏᴏᴜʀ ɪᴅ:</a></b> <code>{message.from_user.id}</code>\n\n"
+
+        if len(message.command) > 1:
+            try:
+                user = await client.get_chat(message.text.split()[1])
+                text += f"<b><emoji id=5321238272406461248>◎</emoji><a href=tg://user?id={user.id}>ᴜsᴇʀ ɪᴅ:</a></b> <code>{user.id}</code>\n\n"
+            except BaseException:
+                return await message.reply("Pengguna tidak ditemukan.")
+
+        text += f"<b><emoji id=5319139772860472979>◎</emoji> <a href=https://t.me/{message.chat.username}>ᴄʜᴀᴛ ɪᴅ:</a></b> <code>{message.chat.id}</code>\n\n"
+
+    if message.reply_to_message:
+        id_ = (
+            message.reply_to_message.from_user.id
+            if message.reply_to_message.from_user
+            else message.reply_to_message.sender_chat.id
+        )
+        file_info = get_file_id(message.reply_to_message)
+        if file_info:
+            text += f"<b><emoji id=5318967574736676420>◎</emoji><a href={message.reply_to_message.link}>ᴍᴇᴅɪᴀ ɪᴅ:</a> <code>{file_info.file_id}</code>\n\n"
+        text += (
+            f"<b><emoji id=5321238272406461248>◎</emoji><a href={message.reply_to_message.link}>ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:</a></b> <code>{message.reply_to_message.id}</code>\n"
+            f"<b><emoji id=5319139772860472979>◎</emoji><a href=tg://user?id={id_}>ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:</a></b> <code>{id_}</code>"
+        )
+
+    return await message.reply(text, disable_web_page_preview=True)
