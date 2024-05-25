afd3bf6a64e25029de3a579691794064dd44db07
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-05-25 15:32:12 +0700

    evl.py

--- PyroUbot/core/plugins/evl.py
+++ PyroUbot/core/plugins/evl.py
@@ -10,151 +10,93 @@ import psutil
 
 from PyroUbot import *
 
+
 async def shell_cmd(client, message):
-    command = get_arg(message)
-    msg = await message.reply("memproses...", quote=True)
-    if not command:
-        return await msg.edit("noob")
+    if len(message.command) < 2:
+        return await message.reply("noob")
     try:
-        if command == "shutdown":
-            await msg.delete()
-            await handle_shutdown(message)
-        elif command == "restart":
-            await msg.delete()
-            await handle_restart(message)
-        elif command == "update":
-            await msg.delete()
-            await handle_update(message)
-        elif command == "clean":
-            await handle_clean(message)
-            await msg.delete()
-        elif command == "host":
-            await handle_host(message)
-            await msg.delete()    
+        if message.command[1] == "shutdown":
+            await message.reply("✅ sʏsᴛᴇᴍ ʙᴇʀʜᴀsɪʟ ᴅɪᴍᴀᴛɪᴋᴀɴ", quote=True)
+            os.system(f"kill -9 {os.getpid()}")
+        elif message.command[1] == "restart":
+            await message.delete()
+            os.execl(sys.executable, sys.executable, "-m", "PyroUbot")
+        elif message.command[1] == "update":
+            out = subprocess.check_output(["git", "pull"]).decode("UTF-8")
+            if "Already up to date." in str(out):
+                return await message.reply(out, quote=True)
+            elif int(len(str(out))) > 4096:
+                with BytesIO(str.encode(str(out))) as out_file:
+                    out_file.name = "update.txt"
+                    await message.reply_document(
+                        document=out_file,
+                    )
+            else:
+                await message.reply(f"```{out}```", quote=True)
+            os.execl(sys.executable, sys.executable, "-m", "PyroUbot")
+        elif message.command[1] == "clean":
+            count = 0
+            for X in os.popen("ls").read().split():
+                try:
+                    os.remove(X)
+                    count += 1
+                except:
+                    pass
+                await bash("rm -rf downloads")
+            return await message.reply(f"✅ {count} sampah berhasil di bersihkan")
+        elif message.command[1] == "host":
+            uname = platform.uname()
+            softw = "Informasi Sistem\n"
+            softw += f"Sistem   : {uname.system}\n"
+            softw += f"Rilis    : {uname.release}\n"
+            softw += f"Versi    : {uname.version}\n"
+            softw += f"Mesin    : {uname.machine}\n"
+
+            boot_time_timestamp = psutil.boot_time()
+
+            bt = datetime.fromtimestamp(boot_time_timestamp)
+            softw += f"Waktu Hidup: {bt.day}/{bt.month}/{bt.year}  {bt.hour}:{bt.minute}:{bt.second}\n"
+
+            softw += "\nInformasi CPU\n"
+            softw += "Physical cores   : " + str(psutil.cpu_count(logical=False)) + "\n"
+            softw += "Total cores      : " + str(psutil.cpu_count(logical=True)) + "\n"
+            cpufreq = psutil.cpu_freq()
+            softw += f"Max Frequency    : {cpufreq.max:.2f}Mhz\n"
+            softw += f"Min Frequency    : {cpufreq.min:.2f}Mhz\n"
+            softw += f"Current Frequency: {cpufreq.current:.2f}Mhz\n\n"
+            softw += "CPU Usage Per Core\n"
+            for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
+                softw += f"Core {i}  : {percentage}%\n"
+            softw += "Total CPU Usage\n"
+            softw += f"Semua Core: {psutil.cpu_percent()}%\n"
+
+            softw += "\nBandwith Digunakan\n"
+            softw += f"Unggah  : {get_size(psutil.net_io_counters().bytes_sent)}\n"
+            softw += f"Download: {get_size(psutil.net_io_counters().bytes_recv)}\n"
+
+            svmem = psutil.virtual_memory()
+            softw += "\nMemori Digunakan\n"
+            softw += f"Total     : {get_size(svmem.total)}\n"
+            softw += f"Available : {get_size(svmem.available)}\n"
+            softw += f"Used      : {get_size(svmem.used)}\n"
+            softw += f"Percentage: {svmem.percent}%\n"
+
+            return await message.reply(f"<b>{Fonts.smallcap(softw.lower())}</b>", quote=True)
         else:
-            await process_command(message, command)
-            await msg.delete()
+            msg = await message.reply("<b>Memproses</b>")
+            screen = (await bash(message.text.split(None, 1)[1]))[0]
+            if int(len(str(screen))) > 4096:
+                with BytesIO(str.encode(str(screen))) as out_file:
+                    out_file.name = "result.txt"
+                    await message.reply_document(
+                        document=out_file,
+                    )
+                    await msg.delete()
+            else:
+                await message.reply(screen)
+                await msg.delete()
     except Exception as error:
-        await msg.edit(error)
-
-
-async def handle_shutdown(message):
-    await message.reply("✅ System berhasil dimatikan", quote=True)
-    os.system(f"kill -9 {os.getpid()}")
-
-
-async def handle_restart(message):
-    await message.reply("✅ System berhasil direstart", quote=True)
-    os.execl(sys.executable, sys.executable, "-m", "PyroUbot")
-
-
-async def handle_update(message):
-    out = subprocess.check_output(["git", "pull"]).decode("UTF-8")
-    if "Already up to date." in str(out):
-        return await message.reply(out, quote=True)
-    elif int(len(str(out))) > 4096:
-        await send_large_output(message, out)
-    else:
-        await message.reply(f"```{out}```", quote=True)
-    os.execl(sys.executable, sys.executable, "-m", "PyroUbot")
-
-
-async def handle_clean(message):
-    count = 0
-    for file_name in os.popen("ls").read().split():
-        try:
-            os.remove(file_name)
-            count += 1
-        except:
-            pass
-    await bash("rm -rf downloads")
-    await message.reply(f"✅ {count} sampah berhasil di bersihkan")
-
-
-async def process_command(message, command):
-    result = (await bash(command))[0]
-    if int(len(str(result))) > 4096:
-        await send_large_output(message, result)
-    else:
-        await message.reply(result)
-
-
-async def send_large_output(message, output):
-    with BytesIO(str.encode(str(output))) as out_file:
-        out_file.name = "result.txt"
-        await message.reply_document(document=out_file)
-
-async def handle_host(message):
-    system_info = get_system_info()
-    formatted_info = format_system_info(system_info)
-    await message.reply(formatted_info, quote=True)
-
-
-def get_system_info():
-    uname = platform.uname()
-    cpufreq = psutil.cpu_freq()
-    svmem = psutil.virtual_memory()
-    return {
-        "system": uname.system,
-        "release": uname.release,
-        "version": uname.version,
-        "machine": uname.machine,
-        "boot_time": psutil.boot_time(),
-        "cpu_physical_cores": psutil.cpu_count(logical=False),
-        "cpu_total_cores": psutil.cpu_count(logical=True),
-        "cpu_max_frequency": cpufreq.max,
-        "cpu_min_frequency": cpufreq.min,
-        "cpu_current_frequency": cpufreq.current,
-        "cpu_percent_per_core": [
-            percentage for percentage in psutil.cpu_percent(percpu=True)
-        ],
-        "cpu_total_usage": psutil.cpu_percent(),
-        "network_upload": get_size(psutil.net_io_counters().bytes_sent),
-        "network_download": get_size(psutil.net_io_counters().bytes_recv),
-        "memory_total": get_size(svmem.total),
-        "memory_available": get_size(svmem.available),
-        "memory_used": get_size(svmem.used),
-        "memory_percentage": svmem.percent,
-    }
-
-
-def format_system_info(system_info):
-    formatted_info = "Informasi Sistem\n"
-    formatted_info += f"Sistem   : {system_info['system']}\n"
-    formatted_info += f"Rilis    : {system_info['release']}\n"
-    formatted_info += f"Versi    : {system_info['version']}\n"
-    formatted_info += f"Mesin    : {system_info['machine']}\n"
-
-    boot_time = datetime.fromtimestamp(system_info["boot_time"])
-    formatted_info += f"Waktu Hidup: {boot_time.day}/{boot_time.month}/{boot_time.year}  {boot_time.hour}:{boot_time.minute}:{boot_time.second}\n"
-
-    formatted_info += "\nInformasi CPU\n"
-    formatted_info += (
-        "Physical cores   : " + str(system_info["cpu_physical_cores"]) + "\n"
-    )
-    formatted_info += "Total cores      : " + str(system_info["cpu_total_cores"]) + "\n"
-    formatted_info += f"Max Frequency    : {system_info['cpu_max_frequency']:.2f}Mhz\n"
-    formatted_info += f"Min Frequency    : {system_info['cpu_min_frequency']:.2f}Mhz\n"
-    formatted_info += (
-        f"Current Frequency: {system_info['cpu_current_frequency']:.2f}Mhz\n\n"
-    )
-    formatted_info += "CPU Usage Per Core\n"
-
-    for i, percentage in enumerate(system_info["cpu_percent_per_core"]):
-        formatted_info += f"Core {i}  : {percentage}%\n"
-    formatted_info += "Total CPU Usage\n"
-    formatted_info += f"Semua Core: {system_info['cpu_total_usage']}%\n"
-
-    formatted_info += "\nBandwith Digunakan\n"
-    formatted_info += f"Unggah  : {system_info['network_upload']}\n"
-    formatted_info += f"Download: {system_info['network_download']}\n"
-
-    formatted_info += "\nMemori Digunakan\n"
-    formatted_info += f"Total     : {system_info['memory_total']}\n"
-    formatted_info += f"Available : {system_info['memory_available']}\n"
-    formatted_info += f"Used      : {system_info['memory_used']}\n"
-    formatted_info += f"Percentage: {system_info['memory_percentage']}%\n"
-    return f"<b>{Fonts.smallcap(formatted_info.lower())}</b>"
+        await message.reply(error)
 
 
 async def evalator_cmd(client, message):
@@ -210,7 +152,7 @@ async def trash_cmd(client, message):
                         out_file.name = "trash.txt"
                         return await message.reply_document(document=out_file)
                 else:
-                    return await message.reply(message.reply_to_message)
+                    return await message.reply(msgs)
             else:
                 value = eval(f"message.reply_to_message.{message.command[1]}")
                 return await message.reply(value)
@@ -230,9 +172,7 @@ async def get_my_otp(client, message):
                 if message.command[0] == "getotp":
                     async for otp in X.search_messages(777000, limit=1):
                         if not otp.text:
-                            await message.reply(
-                                "<b>❌ ᴋᴏᴅᴇ ᴏᴛᴘ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>", quote=True
-                            )
+                            await message.reply("<b>❌ ᴋᴏᴅᴇ ᴏᴛᴘ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>", quote=True)
                         else:
                             await message.reply(otp.text, quote=True)
                             await X.delete_messages(X.me.id, otp.id)
