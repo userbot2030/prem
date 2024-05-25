294b44bfe56ec4fae990745ccf44d1ff1faec10e
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-05-25 16:33:24 +0700

    start.py

--- PyroUbot/modules/start.py
+++ PyroUbot/modules/start.py
@@ -10,3 +10,13 @@ async def _(client, message):
 @PY.PRIVATE
 async def _(client, message):
     await start_cmd(client, message)
+
+
+@PY.BOT("stats")
+async def _(client, message):
+    await stats_ubot(client, message)
+
+
+@PY.CALLBACK("kontol")
+async def _(client, callback_query):
+    await cb_stats(client, callback_query)
