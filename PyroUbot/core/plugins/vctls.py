bdbe8b44959029e7e5084810c56bc15ae448793e
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-05-26 00:56:18 +0700

    vctls.py

--- PyroUbot/core/plugins/vctls.py
+++ PyroUbot/core/plugins/vctls.py
@@ -27,7 +27,6 @@ async def get_group_call(client: Client, message: Message, err_msg: str = "") ->
 
 
 async def joinvc(client, message):
-    gcast_proses = await get_vars(client.me.id, "GCAST_PROSES") or "6113789201717660877"
     gagal = await get_vars(client.me.id, "EMOJI_GAGAL") or "6113891550788324241"
     sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "6111585093220830556"
     msg = await message.reply(f"<b><emoji id={gcast_proses}>⏳</emoji>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ....</b>")
@@ -42,7 +41,6 @@ async def joinvc(client, message):
 
 
 async def leavevc(client: Client, message: Message):
-    gcast_proses = await get_vars(client.me.id, "GCAST_PROSES") or "6113789201717660877"
     gagal = await get_vars(client.me.id, "EMOJI_GAGAL") or "6113891550788324241"
     sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "6111585093220830556"
     msg = await message.reply(f"<b><emoji id={gcast_proses}>⏳</emoji>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ....</b>")
