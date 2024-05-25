d92ee762931d510fb92ed26bb89377d301d57b5e
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-05-25 16:43:55 +0700

    client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -8,7 +8,7 @@ class FILTERS:
     ME = filters.me
     GROUP = filters.group
     PRIVATE = filters.private
-    OWNER = filters.user(5779185981, OWNER_ID)
+    OWNER = filters.user(OWNER_ID)
     ME_GROUP = filters.me & filters.group
     ME_OWNER = filters.me & filters.user(OWNER_ID)
     
